6057fd646984e64e7b8d8f37dfe55c74a5f7b454
Pablo Jurado <pjurado85@gmail.com>
2020-09-13 00:01:19 -0500

    add latest slides and code examples

--- js/examples.js
+++ js/examples.js
@@ -1,5 +1,45 @@
 var app = new Vue({
+  el: "#example0",
+  data: {
+    message: "hello from JS",
+  },
+});
+
+var vueInstance = new Vue({
   el: "#example1",
+  data: {
+    newTodoText: "",
+    todos: [],
+  },
+  methods: {
+    addTodo: function () {
+      this.todos.push(this.newTodoText);
+      this.newTodoText = "";
+    },
+  },
+});
+
+var vueInstance = new Vue({
+  el: "#example2",
+  data: {
+    message: "i'm a lasagna hog",
+  },
+});
+
+var vueInstance = new Vue({
+  el: "#example3",
+  data: {
+    message: "i'm a lasagna hog",
+  },
+  computed: {
+    reversedMessage: function () {
+      return this.message.split("").reverse().join("");
+    },
+  },
+});
+
+var app = new Vue({
+  el: "#example4",
   data: {
     message: "I am using v-model",
   },
